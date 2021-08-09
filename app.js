dad45821a0a553a0c3a391411fde2d66f2b558e5
Fernando Cuervo <63762598+Fcuervo21@users.noreply.github.com>
2021-08-08 21:20:32 -0500

    Alerts were added #10

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express');
+const { check, validationResult } = require('express-validator');
 const app = express();
 const ejs = require('ejs');
 const nodemailer = require('nodemailer');
@@ -10,36 +11,54 @@ app.use(express.static("public")); // Para colocar la carpeta public con css, im
 app.use(express.json());
 
 app.get('/', function(req, res){
-    res.render("home");
+    res.render("home", {
+        alertStatus: undefined,
+        fullName: '',
+        fullEmail: '',
+        fullSubject: '',
+        fullMessage: '',
+    });
 });
 
 app.post('/', function(req, res){
-    console.log(req.body);
+        // const errors = validationResult(req);
+        // if(!errors.isEmpty()){
+        //     // return res.status(400).json({ errors: errors.array() });
+        //     const alert = errors.array();
+        //     res.render('register', {
+        //         alert
+        //     })
+        // }
 
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: process.env.CLIENT_EMAIL,
-            pass: process.env.CLIENT_PASSWORD
+
+        console.log(req.body);
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.CLIENT_EMAIL,
+                pass: process.env.CLIENT_PASSWORD
+            }
+        })
+
+        const mailOptions = {
+            from : req.body.email,
+            to: process.env.CLIENT_EMAIL,//Email de arriba,
+            subject:`Message from ${req.body.email} : ${req.body.subject}`,
+            text: req.body.message
         }
-    })
 
-    const mailOptions = {
-        from : req.body.email,
-        to: process.env.CLIENT_EMAIL,//Email de arriba,
-        subject:`Message from ${req.body.email} : ${req.body.subject}`,
-        text: req.body.message
-    }
-
-    transporter.sendMail(mailOptions, (err, info) => {
-        if(err) {
-            console.error(err);
-            res.send('err');
-        } else {
-            console.log('Email sent: ' + info.response);
-            res.send('success');
-        }
-    })
+        transporter.sendMail(mailOptions, (err, info) => {
+            if(err) {
+                console.error(err);
+                res.send('err');
+            } else {
+                console.log('Email sent: ' + info.response);
+                res.render('home', {alertStatus: true,fullName: '',
+                fullEmail: '',
+                fullSubject: '',
+                fullMessage: ''});
+            }
+        })
 });
 
 app.listen(3000, function () {
--- public/scripts/actionsSubmit.js
+++ public/scripts/actionsSubmit.js
@@ -1,45 +1,60 @@
-// import Swal from 'sweetalert2';
 
-const contactForm = document.querySelector('.contact-form');
-let name = document.getElementById('name');
-let email = document.getElementById('email');
-let subject = document.getElementById('subject');
-let message = document.getElementById('message');
+// // // // import Swal from 'sweetalert2';
 
-contactForm.addEventListener('submit', (e) => {
-    e.preventDefault();
 
-    let formData = {
-        name: name.value,
-        email: email.value,
-        subject: subject.value,
-        message: message.value
-    }
-    let xhr = new XMLHttpRequest();
-    xhr.open('POST', '/');
-    xhr.setRequestHeader('CONTENT-TYPE', 'application/json');
 
-    xhr.onload = function () {
-        console.log(xhr.responseText);
-        if(xhr.responseText === 'success'){
+// const contactForm = document.querySelector('.contact-form');
+// let name = document.getElementById('name');
+// let email = document.getElementById('email');
+// let subject = document.getElementById('subject');
+// let message = document.getElementById('message');
+
+// contactForm.addEventListener('submit', (e) => {
+//     e.preventDefault();
+
+// // //     let formData = {
+// // //         name: name.value,
+// // //         email: email.value,
+// // //         subject: subject.value,
+// // //         message: message.value
+// // //     }
+//     let xhr = new XMLHttpRequest();
+//     xhr.open('POST', '/');
+//     xhr.setRequestHeader('CONTENT-TYPE', 'application/json');
+
+//     xhr.onload = function () {
+// //         console.log(xhr.responseText);
+//         if(xhr.responseText === 'success'){
             // Swal.fire({
             //     icon: 'success',
             //     title: 'Done',
             //     text: 'Email sent successfully!'
             //   });
-            alert('Email sent successfully');
-              name.value = '';
-              email.value = '';
-              subject.value = '';
-              message.value = '';
-        } else {
-            // Swal.fire({
-            //     icon: 'error',
-            //     title: 'Oops..',
-            //     text: 'Something went wrong!'
-            //   });
-            alert('Oops! Something went wrong');
-        }
-    }
-    xhr.send(JSON.stringify(formData));
-})
+//             alert('Email sent successfully');
+            //   name.value = '';
+            //   email.value = '';
+            //   subject.value = '';
+            //   message.value = '';
+//         } else {
+// //             // Swal.fire({
+// //             //     icon: 'error',
+// //             //     title: 'Oops..',
+// //             //     text: 'Something went wrong!'
+// //             //   });
+//             alert('Oops! Something went wrong');
+
+//         }
+//     }
+// //     xhr.send(JSON.stringify(formData));
+// })
+// const contactForm = document.querySelector('.contact-form');
+// contactForm.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     fetch('/').then(response => {
+//         if(response.status === 200) {
+//             alert('Success');
+//         } else {
+//            alert('Error'); 
+//         }
+//     });
+// });
