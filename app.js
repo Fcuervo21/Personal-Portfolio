419d41af10c7ce7c79418a1cf0ecbf8dfcdcc325
Fernando Cuervo <63762598+Fcuervo21@users.noreply.github.com>
2021-08-19 20:39:08 -0500

    Final touch with ports

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const ejs = require('ejs');
 const nodemailer = require('nodemailer');
 
+app.set('port', process.env.PORT || 3000);
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static("public")); // Para colocar la carpeta public con css, img
@@ -79,6 +80,6 @@ app.post('/', [
         }
            
 });
-app.listen(3000, function () {
-    console.log("Server started on port 3000");
+app.listen(app.get('port'), function () {
+    console.log("Server started on port", app.get('port'));
 });
